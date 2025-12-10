f3c70c73f24e95e0f1781eaebc81ef4b77e18968
cehao <cehao@u.nus.edu>
2025-12-10 18:03:08 +0800

    seems reasonable

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # training 
 
-python train.py --max_epochs 2000 --patience 100 
+python train.py --max_epochs 4000 --patience 200 
 
-# testing
-python eval.py --ckpt "runs/20251210_174147/checkpoint_best.pt" --n_samples 10
+# testing, change the checkpoint path as needed
+python eval.py --ckpt "runs/20251210_175945/checkpoint_best.pt" --n_samples 1000 --n_steps 500
